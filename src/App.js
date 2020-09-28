f56c659b2606b907c7a7cb28201c1937c2014ec2
Talia <hm.talia@gmail.com>
2020-09-28 10:07:29 -0700

    images

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import pictures from './external';
+
 class App extends React.Component {
   render() {
   return (
     <div className="App">
      <h1>🤝 I have won React! 🌟</h1>
+     {
+       pictures.map((pic) => {
+         return (
+           <img src={pic.url} />
+         )
+       })
+     }
     </div>
   );
   }
---
+++ src/external.js
@@ -0,0 +1,10 @@
+const pictures = [
+    {
+        url: require('./assets/one.jpg'),
+    },
+    {
+        url: require('./assets/two.jpg'),
+    }
+]
+
+export default pictures;
