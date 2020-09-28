bab6eaf75e1616e373dc0bc0977339eda99a9d6f
Talia <hm.talia@gmail.com>
2020-09-28 09:20:45 -0700

    Updates

---
+++ precache-manifest.5e6d981e37b40d7e63da3689fbf9eeed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933ac730df2013fe2f574d5002d39d22",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "0f4aae78defaea0652cf",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5b720cb6ada735e792b",
+    "url": "/deploy-test/static/js/2.3bbb2c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.3bbb2c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4aae78defaea0652cf",
+    "url": "/deploy-test/static/js/main.a9384310.chunk.js"
+  },
+  {
+    "revision": "23db0319da0c4e8793b9",
+    "url": "/deploy-test/static/js/runtime-main.d3bbeae5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
